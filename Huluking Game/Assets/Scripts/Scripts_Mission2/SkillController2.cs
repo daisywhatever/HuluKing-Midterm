1a4f24bbaf98395e1e24a489e5057d1b208c5b09
daisywhatever <xinfang@XinFangs-MacBook-Pro.local>
2016-11-12 15:01:56 -0800

    Mission2 Update

---
+++ Huluking Game/Assets/Scripts/Scripts_Mission2/SkillController2.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class SkillController2 : MonoBehaviour,IPointerDownHandler {
+
+	public GameObject player;
+	public PlayerController playController;
+	public Camera cam;
+	private Image skillImg;
+	private bool flag;
+
+	// Use this for initialization
+	private void Start() {
+		flag = false;
+		skillImg = GetComponent<Image> ();
+		if (player != null) {
+			playController = player.GetComponent<PlayerController> ();
+		}
+	}
+
+	void RedSkill()
+	{
+		
+		Vector3 position = cam.transform.position;
+		if (flag) {
+			player.transform.localScale = new Vector3 (1, 1, 1);
+			flag = false;
+		} else {
+			player.transform.localScale = new Vector3 (4, 4, 4);
+			flag = true;
+		}
+		cam.transform.position = position;
+	}
+
+	public virtual void OnPointerDown(PointerEventData ped)
+	{
+		
+		Vector2 pos;
+		if (RectTransformUtility.ScreenPointToLocalPointInRectangle (skillImg.rectTransform, ped.position, ped.pressEventCamera, out pos)) {
+			RedSkill ();
+		}
+
+	}
+}
