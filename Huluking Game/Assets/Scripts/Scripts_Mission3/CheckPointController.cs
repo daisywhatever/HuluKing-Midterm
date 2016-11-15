e2c9f854a8a03d87da27bff23beab7e5d5636f58
Feng Wang <wang167@usc.edu>
2016-11-14 22:39:58 -0800

    update the mission 3 and fix the bugs

--- Huluking Game/Assets/Scripts/Scripts_Mission3/CheckPointController.cs
+++ Huluking Game/Assets/Scripts/Scripts_Mission3/CheckPointController.cs
@@ -6,7 +6,7 @@ public class CheckPointController : MonoBehaviour {
 	public GameObject player;
 	// Use this for initialization
 	void OnTriggerEnter() {
-		Debug.Log ("Checkpoint triggered.");
+		
 		PlayerController3 controller = player.GetComponent<PlayerController3> ();
 		controller.setCheckPoint(this.GetComponent<Collider>());
 	}
